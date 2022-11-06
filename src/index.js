a8d5b55c24250055042c42b98ae38cad2f6dc9c7
rafaelBPorto <rafaelbporto88@gmail.com>
2022-11-06 15:24:51 -0300

    Post /sign-up recebendo requisição do usuário para entrar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import express from 'express'
+import cors from 'cors'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const users = [
+    // {
+    //     username: 'bobesponja',
+    //     avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
+    // }
+]
+
+const tweets =[]
+
+app.post('/sign-up', (req, res) => {
+    users.push(req.body)
+    res.send("OK")
+
+})
+
+app.listen(5000)
