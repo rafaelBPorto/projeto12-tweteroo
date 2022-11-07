68bb6d85a830c8506a26488523d17b29dd630c58
rafaelBPorto <rafaelbporto88@gmail.com>
2022-11-06 21:13:21 -0300

    V2.0.0 Remoção dos dados mockados, alteração na logica do for em GET /tweets

--- src/index.js
+++ src/index.js
@@ -1,116 +1,54 @@
 import express from 'express'
 import cors from 'cors'
 
-const app = express()
-app.use(express.json())
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-const users = [
-    {
-        username: 'bobesponja',
-        avatar: "https://img.elo7.com.br/product/original/3254FDB/bob-esponja-e-patrick-em-camadas-arquivo-de-corte-personalizados-bob-esponja-e-patrick.jpg"
-    },
-    {
-        username: 'patrick',
-        avatar: "http://s2.glbimg.com/nCXjcf8JWf3LtDMLvB_Ele-giGo=/290x386/s2.glbimg.com/8UQomHbV_oPsS2XIr9oou8MTJSk=/s.glbimg.com/jo/g1/f/original/2013/07/06/patrick_1.jpg"
-    }
-]
-
-const tweets = [
-    {
-        username: "b",
-        tweet: "1"
-    },
-    {
-        username: "patrick",
-        tweet: "2"
-    },
-    {
-        username: "patrick",
-        tweet: "3"
-    },
-    {
-        username: "patrick",
-        tweet: "4"
-    },
-    {
-        username: "patrick",
-        tweet: "5"
-    },
-    {
-        username: "patrick",
-        tweet: "6"
-    },
-    {
-        username: "patrick",
-        tweet: "7"
-    },
-    {
-        username: "patrick",
-        tweet: "8"
-    },
-    {
-        username: "patrick",
-        tweet: "9"
-    },
-    {
-        username: "patrick",
-        tweet: "10"
-    },
-    {
-        username: "patrick",
-        tweet: "11"
-    },
-    {
-        username: "patrick",
-        tweet: "12"
-    },
-    {
-        username: "patrick",
-        tweet: "13"
-    }
-]
-
+const users = [];
+const tweets = [];
 
 app.post('/sign-up', (req, res) => {
 
     if (!req.body.username || !req.body.avatar) {
-        res.status(400).send("Todos os campos são obrigatórios")
+        res.status(400).send("Todos os campos são obrigatórios!");
     }
-    users.push(req.body)
-    res.send("OK")
+    users.push(req.body);
+    res.send("OK");
 
+})
+
+app.get('/sign-up', (req, res) => {
+    res.send(users)
 })
 
 app.post('/tweets', (req, res) => {
     if (!req.body.tweet) {
-        res.status(400).send("É obrigatório escrever algo")
+        res.status(400).send("Todos os campos são obrigatórios!");
     }
-    tweets.push(req.body)
-    res.send("OK")
+    tweets.push(req.body);
+    res.status(201).send("OK");
 })
 
 app.get('/tweets', (req, res) => {
-    const response = []
-    const lastTweet = tweets.length - 1
-    const stopTweet = lastTweet - 10
+    const response = [];
+    const lastTweet = tweets.length - 1;
+    const stopTweet = lastTweet - 10;
 
-
-    for (let i = lastTweet, k = 0; i > stopTweet; i--, k++) {
-        console.log(i)
-        const positionUser = users.findIndex(element => tweets[i].username === element.username)
-        response.push(tweets[i])
-        if (positionUser > -1) {
-            const avatar = users[positionUser].avatar
-            response[k].avatar = avatar
-        } else {
-            response[k].avatar = ""
+    if (tweets.length > 0) {
+        for (let i = lastTweet, k = 0; i > stopTweet && i >=0; i--, k++) {
+            const positionUser = users.findIndex(element => tweets[i].username === element.username);
+            response.push(tweets[i]);
+            if (positionUser > -1) {
+                const avatar = users[positionUser].avatar;
+                response[k].avatar = avatar;
+            } else {
+                response[k].avatar = "";
+            }
         }
-
     }
 
-    console.log(response)
-    res.send(response)
+    res.send(response);
 });
 
 app.listen(5000)
